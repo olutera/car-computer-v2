b946c90d8a0d69912303d6dc0b38448ff5145671
olutera <olutera@gmail.com>
2015-05-29 23:06:39 +0200

    Atmel Studio 6.1 project files added
    Signed-off-by: olutera <olutera@gmail.com>

---
+++ Release/makedep.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc.c
+
+buttons.c
+
+car_meas.c
+
+delay.c
+
+ds18b20.c
+
+hd44780.c
+
+main.c
+
+pwr_down.c
+
+temp.c
+
+uart.c
+
+wdt_timer.c
+
